f6e08d159d56d53b93454727ae35ec1616c08350
Joshuacosta <joshuacosta6@gmail.com>
2015-01-26 18:26:51 +0100

    Initial commit

---
+++ app/src/main/java/com/example/it00046/drawingfun/DrawingView.java
@@ -0,0 +1,21 @@
+package com.example.it00046.drawingfun;
+
+import android.view.View;
+import android.content.Context;
+import android.util.AttributeSet;
+
+/**
+ * Created by it00046 on 26/01/2015.
+ */
+public class DrawingView extends View {
+
+    public DrawingView(Context context, AttributeSet attrs){
+        super(context, attrs);
+        setupDrawing();
+    }
+
+    private void setupDrawing(){
+        //get drawing area setup for interaction
+
+    }
+}
